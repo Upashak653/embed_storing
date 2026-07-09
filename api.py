d27f99d968c7082968e9523c96290348891ef417
unknown <QBA SAP@INKOLQBA0201.qbadvisory.com>
2026-07-09 13:27:17 +0530

    fix: use starlette CORSMiddleware

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 # ── Imports ─────────────────────────────────────────────────────────────────
 from fastapi                    import FastAPI
-from fastapi.middleware.cors    import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from fastapi.concurrency        import run_in_threadpool
 from pydantic                   import BaseModel
 from typing                     import Literal
@@ -15,15 +15,12 @@ import psycopg2
 app = FastAPI()
 
 app.add_middleware(
-    app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-)
-
 # ── Pydantic models ───────────────────────────────────────────────────────────
 
 class SearchRequest(BaseModel):
